e8b605052595fcc5f6d483d6f4c3678ce85069b0
joseconejoo <39308405+joseconejoo@users.noreply.com>
2021-02-21 12:07:36 -0400

    Update

--- src/google/footer.js
+++ src/google/footer.js
@@ -8,6 +8,17 @@ class Footer1 extends Component {
                 <div className='Country'> 
                     Venezuela
                 </div>
+                <div>
+                </div>
+                <div className='footer_mid2'>
+                    <div className="footer_mid">
+                        <a className="foot_ext" href="https://about.google/?utm_source=google-VE&amp;utm_medium=referral&amp;utm_campaign=hp-footer&amp;fg=1" ping="/url?sa=t&amp;rct=j&amp;source=webhp&amp;url=https://about.google/%3Futm_source%3Dgoogle-VE%26utm_medium%3Dreferral%26utm_campaign%3Dhp-footer%26fg%3D1&amp;ved=0ahUKEwihhsHBk_vuAhVCrFkKHUofDKoQkNQCCA8">Sobre Google</a>
+                        <a className="foot_ext" href="https://www.google.com/intl/es_ve/ads/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&amp;utm_source=google.com&amp;utm_medium=referral&amp;utm_campaign=google_hpafooter&amp;fg=1" ping="/url?sa=t&amp;rct=j&amp;source=webhp&amp;url=https://www.google.com/intl/es_ve/ads/%3Fsubid%3Dww-ww-et-g-awa-a-g_hpafoot1_1!o2%26utm_source%3Dgoogle.com%26utm_medium%3Dreferral%26utm_campaign%3Dgoogle_hpafooter%26fg%3D1&amp;ved=0ahUKEwihhsHBk_vuAhVCrFkKHUofDKoQkdQCCBA">Publicidad</a>
+                        <a className="foot_ext" href="https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&amp;utm_source=google.com&amp;utm_medium=referral&amp;utm_campaign=google_hpbfooter&amp;fg=1" ping="/url?sa=t&amp;rct=j&amp;source=webhp&amp;url=https://www.google.com/services/%3Fsubid%3Dww-ww-et-g-awa-a-g_hpbfoot1_1!o2%26utm_source%3Dgoogle.com%26utm_medium%3Dreferral%26utm_campaign%3Dgoogle_hpbfooter%26fg%3D1&amp;ved=0ahUKEwihhsHBk_vuAhVCrFkKHUofDKoQktQCCBE">Negocios</a>
+                        <a className="foot_ext" href="https://google.com/search/howsearchworks/?fg=1">  Cómo funciona la Búsqueda </a>
+                    </div>
+                </div>
+
             </footer>
         </div>
     }
